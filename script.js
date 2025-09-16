7f36b4443aace027f61fe24856839e2cc10c137d
Versalign <aknotts@versalign.com>
2025-09-16 10:12:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -43,11 +43,5 @@ window.addEventListener('DOMContentLoaded', function() {
     var nav = document.querySelector(".topnav");
     nav.classList.toggle("responsive");
  
-// Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
 }
+
